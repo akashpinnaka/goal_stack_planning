bc1b6375a89c6007816ca53cc798996ce609b8dc
akashpinnaka <aakash.pinnaka@gmail.com>
2018-12-04 18:38:17 -0500

    Completed

--- script.js
+++ script.js
@@ -48,40 +48,64 @@ var init_slots = []
 var final_slots = []
 
 // from UI
-var first_location_input = []
-var second_location_input = []
-var third_location_input = []
-var fourth_location_input = []
-var first_location_output = []
-var second_location_output = []
-var third_location_output = []
-var fourth_location_output = []
-$("#take_data").on('click', function() {
-  console.log("******************")
-  first_location_input = $("#init-location-1").val().split(",");
-  second_location_input = $("#init-location-2").val().split(",");
-  third_location_input = $("#init-location-3").val().split(",");
-  fourth_location_input = $("#init-location-4").val().split(",");
-  first_location_output = $("#final-location-1").val().split(",");
-  second_location_output = $("#final-location-2").val().split(",");
-  third_location_output = $("#final-location-3").val().split(",");
-  fourth_location_output = $("#final-location-4").val().split(",");
-  init_slots.push(first_location_input, second_location_input, third_location_input,fourth_location_input)
-  final_slots.push(first_location_output, second_location_output, third_location_output, fourth_location_output)
-});
+// var first_location_input = []
+// var second_location_input = []
+// var third_location_input = []
+// var fourth_location_input = []
+// var first_location_output = []
+// var second_location_output = []
+// var third_location_output = []
+// var fourth_location_output = []
+// $("#take_data").on('click', function() {
+//   first_location_input = $("#init-location-1").val().split(",");
+//   second_location_input = $("#init-location-2").val().split(",");
+//   third_location_input = $("#init-location-3").val().split(",");
+//   fourth_location_input = $("#init-location-4").val().split(",");
+//   first_location_output = $("#final-location-1").val().split(",");
+//   second_location_output = $("#final-location-2").val().split(",");
+//   third_location_output = $("#final-location-3").val().split(",");
+//   fourth_location_output = $("#final-location-4").val().split(",");
+//   init_slots.push(first_location_input, second_location_input, third_location_input,fourth_location_input)
+//   final_slots.push(first_location_output, second_location_output, third_location_output, fourth_location_output)
+
+//   for(var i = 0; i < init_slots.length; i++) {
+//     for(var j = 0; j < i.length; j++) {
+//       init_loc[j] = i
+//     }
+//   }
+
+//   console.log(init_slots)
+//   console.log(init_loc)
+
+//   console.log("***************************************")
+//   console.log(init_on)
+//   console.log("***************************************")
+
+//   for(var i = 0; i < init_slots.length; i++) {
+//     for(var j = 0; j < i.length; j++) {
+//       if(j == 0) {
+//         init_on[j] = "table"
+//       }
+//       else {
+//         init_on[j] = i[j-1]
+//       }
+//     }
+//   }
+
+//   for(var i = 0; i < init_slots.length; i++) {
+//     for(var j = 0; j < i.length; j++) {
+//       if(j == i.length - 1) {
+//         init_top[j] = "clear"
+//       }
+//       else {
+//         init_top[j] = i[j+1]
+//       }
+//     }
+//   }
+// });
 
 //End of from UI
 
-for(var i = 0; i < init_slots.length; i++) {
-  for(var j = 0; j < i.length; j++) {
-    if(j == 0) {
-      init_on[j] = "table"
-    }
-    else {
-      init_on[j] = 
-    }
-  }
-}
 
 // init_slots  = JSON.parse('[["A","B","C","D","E","F","G","H","I","J"],["K"],["L"],["M","N"]]')
 // final_slots = JSON.parse('[["N","J","I","H","G","F","E","D","C","B","A"],["M"],["L"],["K"]]')
@@ -103,101 +127,71 @@ for(var i = 0; i < init_slots.length; i++) {
 console.log(init_slots)
 console.log(final_slots)
 
-init_on = JSON.parse('{"A":"table","B":"A","C":"B","D":"C","E":"D","F":"E","G":"F","H":"G","I":"H","J":"I","K":"table","L":"table","M":"table","N":"M"}')
-init_loc = JSON.parse('{"A":"1","B":"1","C":"1","D":"1","E":"1","F":"1","G":"1","H":"1","I":"1","J":"1","K":"2","L":"3","M":"4","N":"4"}')
-init_top = JSON.parse('{"A":"B","B":"C","C":"D","D":"E","E":"F","F":"G","G":"H","H":"I","I":"J","J":"clear","K":"clear","L":"clear","M":"N","N":"clear"}')
+// init_on = JSON.parse('{"A":"table","B":"A","C":"B","D":"C","E":"D","F":"E","G":"F","H":"G","I":"H","J":"I","K":"table","L":"table","M":"table","N":"M"}')
+// init_loc = JSON.parse('{"A":"1","B":"1","C":"1","D":"1","E":"1","F":"1","G":"1","H":"1","I":"1","J":"1","K":"2","L":"3","M":"4","N":"4"}')
+// init_top = JSON.parse('{"A":"B","B":"C","C":"D","D":"E","E":"F","F":"G","G":"H","H":"I","I":"J","J":"clear","K":"clear","L":"clear","M":"N","N":"clear"}')
 
-final_on= JSON.parse('{"A":"B","B":"C","C":"D","D":"E","E":"F","F":"G","G":"H","H":"I","I":"J","J":"N","K":"table","L":"table","M":"table","N":"table"}')
-final_loc= JSON.parse('{"A":"1","B":"1","C":"1","D":"1","E":"1","F":"1","G":"1","H":"1","I":"1","J":"1","K":"4","L":"3","M":"2","N":"1"}')
-final_top= JSON.parse('{"A":"clear","B":"A","C":"B","D":"C","E":"D","F":"E","G":"F","H":"G","I":"H","J":"I","K":"clear","L":"clear","M":"clear","N":"J"}')
+// final_on= JSON.parse('{"A":"B","B":"C","C":"D","D":"E","E":"F","F":"G","G":"H","H":"I","I":"J","J":"N","K":"table","L":"table","M":"table","N":"table"}')
+// final_loc= JSON.parse('{"A":"1","B":"1","C":"1","D":"1","E":"1","F":"1","G":"1","H":"1","I":"1","J":"1","K":"4","L":"3","M":"2","N":"1"}')
+// final_top= JSON.parse('{"A":"clear","B":"A","C":"B","D":"C","E":"D","F":"E","G":"F","H":"G","I":"H","J":"I","K":"clear","L":"clear","M":"clear","N":"J"}')
 
 
-$(document).ready(function () {
+init_on = JSON.parse('{"A":"table","B":"A","K":"B","L":"K","M":"L","N":"M","D":"C","F":"E","G":"F","I":"H","H":"table","C":"table","E":"table","J":"I"}')
+init_loc = JSON.parse('{"A":"1","B":"1","C":"2","D":"2","E":"3","F":"3","G":"3","H":"4","I":"4","J":"4","K":"1","L":"1","M":"1","N":"1"}')
+init_top = JSON.parse('{"A":"B","B":"K","K":"L","L":"M","M":"N","C":"D","E":"F","F":"G","H":"I","J":"clear","G":"clear","D":"clear","I":"J","N":"clear"}')
 
-  
+final_on= JSON.parse('{"A":"B","B":"C","C":"F","F":"E","E":"D","D":"G","G":"H","H":"I","I":"J","J":"N","K":"table","L":"table","M":"table","N":"table"}')
+final_loc= JSON.parse('{"A":"1","B":"1","C":"1","F":"1","E":"1","D":"1","G":"1","H":"1","I":"1","J":"1","K":"4","L":"3","M":"2","N":"1"}')
+final_top= JSON.parse('{"A":"clear","B":"A","C":"B","F":"C","E":"F","D":"E","G":"D","H":"G","I":"H","J":"I","K":"clear","L":"clear","M":"clear","N":"J"}')
+
+$(document).ready(function () {  
   var a = $("#init_loc_table option:selected").val()
-  $("#init").append('<div  class="col-lg-3" id="init_loc1"> LOCATION ' + 1 + ' <br><br></div>')
-
-  
+  $("#init").append('<div  class="col-lg-3" id="init_loc1"> LOCATION ' + 1 + ' <br><br></div>')  
   var a = $("#final_loc_table option:selected ").val()
-  $("#final").append('<div class="col-lg-3" id="final_loc1"> LOCATION ' + 1 + ' <br><br></div>')
-
-  
+  $("#final").append('<div class="col-lg-3" id="final_loc1"> LOCATION ' + 1 + ' <br><br></div>')  
   $("#init_loc_table").change(function () {
-
-    var a = $("#init_loc_table option:selected ").val()
-
-    
+    var a = $("#init_loc_table option:selected ").val()    
     if (!($('#init_loc' + a + '').length)) {
-
       $("#init").append('   <div class="col-lg-3" id="init_loc' + a + '"> LOCATION ' + a + ' <br><br></div>')
-
-
     }
- 
 });
-
-
-
   
   $("#final_loc_table").change(function () {
-
-
-    var a = $("#final_loc_table option:selected ").val()
-
-    
+    var a = $("#final_loc_table option:selected ").val()    
     if (!($('#final_loc' + a + '').length))
       $("#final").append(' <div  class="col-lg-3" id="final_loc' + a + '"> LOCATION ' + a + ' <br><br></div>')
 
   });
 
-
-
-  
   function count_blocks_at_loc_table(variable1, variable2, location) {
     var count = 0;
     Object.keys(variable1).forEach(function (key) {
       if (variable1[key] == "table")
         if (variable2[key] == location)
           count = count + 1;
-
     });
     return count
   }
 
-
-
-  
   function count_blocks_on_table(variable) {
     var count = 0;
     Object.keys(variable).forEach(function (key) {
       if (variable[key] == "table")
         count = count + 1;
-
     });
     return count
   }
 
-
-  $("#init_table_click").click(function () {
-
-    
+  $("#init_table_click").click(function () {    
     var a = $("#init_table ").val()
-    var loc = $("#init_loc_table option:selected ").val()
-
-    
+    var loc = $("#init_loc_table option:selected ").val()    
     if (init_on[a])
-      alert("You cannot insert the BLOCK(" + a + ") on Table it is already on BLOCK(" + init_on[a] + ")")
-
-
-    
+      alert("You cannot insert the BLOCK(" + a + ") on Table it is already on BLOCK(" + init_on[a] + ")")    
     else if (count_blocks_at_loc_table(init_on, init_loc, loc))
       alert("There is already a block on table at this location you cannot insert more blocks on table")
-
     else {
       if (count_blocks_on_table(init_on) >= 4)
         alert("You cannot insert the BLOCK(" + a + ") on Table there are already 4 of the blocks on table")
-
       else {
         init_on[a] = "table"
         init_loc[a] = loc
@@ -205,18 +199,11 @@ $(document).ready(function () {
         $("#init_loc" + loc).append("<span style='padding-left:20%;font-weight:normal'>TABLE(" + a + ")</span><br><br>")
       }
     }
-
   });
 
-
-
-  $("#final_table_click").click(function () {
-    
+  $("#final_table_click").click(function () {    
     var a = $("#final_table ").val()
-    var loc = $("#final_loc_table option:selected ").val()
-
-
-    
+    var loc = $("#final_loc_table option:selected ").val()    
     if (final_on[a])
       alert("You cannot insert the BLOCK(" + a + ") on Table it is already on BLOCK(" + final_on[a] + ")")
 
@@ -280,38 +267,22 @@ $(document).ready(function () {
 
   });
 
-
-
-
-
   $("#final_on_click").click(function () {
-
     var a = $("#final_table_1 ").val()
     var b = $("#final_table_2 ").val()
     var loc = $("#final_loc_table option:selected ").val()
     
     if (a == b)
-      alert("A BLOCK CANNOT BE ON THE SAME BLOCK")
-
-    
+      alert("A BLOCK CANNOT BE ON THE SAME BLOCK")    
     else if (final_on[a])
-      alert("You cannot keep BLOCK(" + a + ") on BLOCK(" + b + ") beacause it is on " + final_on[a])
-
-    
+      alert("You cannot keep BLOCK(" + a + ") on BLOCK(" + b + ") beacause it is on " + final_on[a])    
     else if (final_top[b] != "clear")
       alert("BLOCK(" + b + ") already has top  BLOCK(" + final_top[b] + ") you cannot insert BLOCK(" + a + ")")
-
-    
-
     else if (final_loc[b] != loc && final_loc[b]) {
       alert("You cannot place a BLOCK(" + a + ") on  BLOCK(" + b + ") which is at location " + final_loc[b] + " but you are in location " + loc)
-
     }
-
     else if (!(final_loc[b]))
-      alert("First enter BLOCK(" + b + ") on table ")
-
-    
+      alert("First enter BLOCK(" + b + ") on table ")    
     else {
       final_on[a] = b
       final_loc[a] = loc
@@ -319,17 +290,9 @@ $(document).ready(function () {
       final_slots[loc-1].push(a)
       $("#final_loc" + loc).append("<span style='padding-left:20%;font-weight:normal'>ON(" + a + " , " + b + ")</span><br><br>")
     }
-
-
   });
 
-  
-
   function check_incomplete_information(init_on, final_on) {
-
-    
-    
-    
     init_incomp = [], final_incomp = []
     console.group("init : ")
     console.log(init_on)
@@ -658,4 +621,10 @@ $(document).ready(function () {
       $("#results").append("<h4>" + plan_result[i] + "</h4>");
     }
   }); 
+});
+
+$(document).ready(function() {
+  $("#present").on('click', function() {
+    $("#presentation").show();
+  });
 });
